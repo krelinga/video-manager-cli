379065535f6ad67c143e6ab26407cbc41399d1da
Andy Kreling <krelinga@gmail.com>
2025-11-16 08:58:22 +0000

    get ListInbox command working.
    lots of cleanup still to do.

--- cmd/root.go
+++ cmd/root.go
@@ -1,30 +1,49 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/spf13/cobra"
+
+	"buf.build/gen/go/krelinga/proto/connectrpc/go/krelinga/video_manager/disc/v1/discv1connect"
+	discv1 "buf.build/gen/go/krelinga/proto/protocolbuffers/go/krelinga/video_manager/disc/v1"
+	"connectrpc.com/connect"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "video-manager-cli",
-	Short: "A brief description of your application",
+	Use:   "vm",
+	Short: "video-manager CLI tool",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: run,
+}
+
+func run(cmd *cobra.Command, args []string) {
+	fmt.Println("connecting to server address:", serverAddr)
+	client := discv1connect.NewDiscServiceClient(http.DefaultClient, serverAddr)
+
+	req := connect.NewRequest(&discv1.ListInboxRequest{})
+	resp, err := client.ListInbox(cmd.Context(), req)
+	if err != nil {
+		fmt.Println("error listing inbox:", err)
+		os.Exit(1)
+		return
+	}
+
+	fmt.Println("inbox videos:")
+	for _, dir := range resp.Msg.Directories {
+		fmt.Printf("- %s\n", dir)
+	}
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -36,16 +55,8 @@ func Execute() {
 	}
 }
 
+var serverAddr string
+
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.video-manager-cli.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().StringVar(&serverAddr, "server", "", "server address")
 }
-
-
--- go.mod
+++ go.mod
@@ -2,9 +2,15 @@ module github.com/krelinga/video-manager-cli
 
 go 1.24.9
 
-require github.com/spf13/cobra v1.10.1
+require (
+	buf.build/gen/go/krelinga/proto/connectrpc/go v1.19.1-20251116064153-1bb37e38d2c4.2
+	buf.build/gen/go/krelinga/proto/protocolbuffers/go v1.36.10-20251116064153-1bb37e38d2c4.1
+	connectrpc.com/connect v1.19.1
+	github.com/spf13/cobra v1.10.1
+)
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.9 // indirect
+	google.golang.org/protobuf v1.36.10 // indirect
 )
